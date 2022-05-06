87b5f166426d4841edec5379d3db8aa5b13bcad1
xcapde <xcapde@hotmail.com>
2022-05-06 11:37:04 +0200

    css import

--- scripts/search.js
+++
@@ -1,8 +0,0 @@
-import { toDoList } from"./toDoList.js"
-
-let searchButton = document.getElementById(`buttonSearch`);
-let searchInput = document.getElementById('input_search');
-console.log(searchInput.value)
-
-
-toDoList
